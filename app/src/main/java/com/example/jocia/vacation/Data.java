39497f6267b0e8e6822bebb7a70c65fc8cdbeabc
jocianoperin <38639676+jocianoperin@users.noreply.github.com>
2018-10-23 11:55:08 -0300

    Criação do botão voltar nas activitys

--- app/src/main/java/com/example/jocia/vacation/Data.java
+++ app/src/main/java/com/example/jocia/vacation/Data.java
@@ -1,36 +1,55 @@
 package com.example.jocia.vacation;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.Calendar;
 
 public class Data extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_data);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
+        getSupportActionBar().setTitle("Data");
 
     }
-    public void showCalendar(View view) {
+    public void showCalendar() {
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, MainActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){ //Botão BACK padrão do android
+        startActivity(new Intent(this, MainActivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 
     public void toLocal(View view) {
 
         Intent intent = new Intent(this, Local.class);
         startActivity(intent);
+    }
+
+    public void showCalendar(View view) {
+
     }
 }
--- app/src/main/java/com/example/jocia/vacation/Local.java
+++ app/src/main/java/com/example/jocia/vacation/Local.java
@@ -1,9 +1,9 @@
 package com.example.jocia.vacation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 public class Local extends AppCompatActivity {
 
@@ -11,6 +11,10 @@ public class Local extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
+        getSupportActionBar().setTitle("Local");
 
         PontosTuristicos[] criaPontos = new PontosTuristicos[10];
 
@@ -24,5 +28,24 @@ public class Local extends AppCompatActivity {
         criaPontos[7] = new PontosTuristicos("Dois Vizinhos", "Inverno");
         criaPontos[8] = new PontosTuristicos("Quedas do Iguaçu", "Verão");
         criaPontos[9] = new PontosTuristicos("Dois Vizinhos", "Verão");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, MainActivity.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){ //Botão BACK padrão do android
+        startActivity(new Intent(this, MainActivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 }
